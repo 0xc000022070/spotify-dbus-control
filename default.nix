c3af03bb5cec7b982a0646b4ec1f1406c098d2c8
Luis Quiñones <lpaandres2020@gmail.com>
2024-05-25 15:10:57 -0500

    nix: added package builder

---
+++ default.nix
@@ -0,0 +1,29 @@
+{pkgs ? import <nixpkgs> {}, ...}:
+pkgs.stdenv.mkDerivation rec {
+  name = "spotify-dbus";
+
+  src = builtins.path {
+    name = "${name}-source";
+    path = ./.;
+  };
+
+  nativeBuildInputs = [pkgs.makeWrapper];
+
+  propagatedBuildInputs = [
+    pkgs.dbus
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin/
+    cp ./main.sh $out/bin/${name} && chmod +x $out/bin/${name}
+
+    runHook postInstall
+  '';
+
+  postInstall = ''
+    wrapProgram ${placeholder "out"}/bin/${name} \
+      --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.dbus]}
+  '';
+}
